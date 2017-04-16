857aa30fd2b40b09ece340c149a9fc94e2952393
cesarlopezfa <cesarlopezfa@gmail.com>
2017-04-16 23:00:00 +0200

    Include inputs to register weight & list them

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="app">
+
+        <div>
+            <label>Add your weight</label>
+            <input type="number" placeholder="U Weight" v-model="weight">
+            <input type="text" placeholder="dd/mm/yyyy (optional)" v-model="created">
+            <button @click="addWeight">Add</button>
+        </div>
+
+        <div>
+            <h1>Weight History</h1>
+            <ul>
+                <li v-for="w in weights">
+                    <span>{{ w.value }}</span><span> - </span><span>{{ w.created | date }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        computed: {
+            weight: {
+                get () {
+                    return this.$store.state.weight
+                },
+                set (value) {
+                    this.$store.commit('updateWeight', value)
+                }
+            },
+            created: {
+                get () {
+                    return this.$store.state.created
+                },
+                set (value) {
+                    this.$store.commit('updateCreated', value)
+                }
+            },
+            weights () {
+                return this.$store.getters.weights
+            }
+        },
+        methods: {
+            addWeight () {
+                this.$store.dispatch('persistWeight');
+            }
+        }
+    }
+</script>
+
+<style{{#sass}} lang="scss"{{/sass}}>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,82 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+import axios from 'axios';
+import moment from 'moment';
+
+
+Vue.use(Vuex);
+
+Vue.filter('date', function(value) {
+   return moment(value).format('DD/MM/YYYY');
+});
+
+const store = new Vuex.Store({
+    state: {
+        weight: undefined,
+        created: undefined,
+        weights: []
+    },
+    getters: {
+        weights: state => {
+            return state.weights;
+        }
+    },
+    mutations: {
+        updateWeight (state, value) {
+            state.weight = value;
+        },
+        updateCreated (state, value) {
+            state.created = value;
+        },
+        updateWeights (state, weights) {
+            state.weights = weights;
+            state.weights.sort(function(a, b) {
+                if (a.created < b.created) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        },
+        reset (state) {
+            state.weight = undefined;
+            state.created = undefined;
+        }
+    },
+    actions: {
+        persistWeight (context) {
+            let date;
+
+            if (context.state.created !== undefined) {
+                date = moment(context.state.created, 'DD/MM/YYYY');
+            }
+
+            axios.post('/api/weights', {
+                value: context.state.weight,
+                created: date
+            }).then(function() {
+                context.dispatch('reset');
+                context.dispatch('retrieveWeights');
+            });
+
+        },
+        retrieveWeights (context) {
+            axios.get('/api/weights').then(function(response) {
+                context.commit('updateWeights', response.data);
+            });
+
+        }
+    }
+});
+
+const vm = new Vue({
+        el: '#app',
+        render: h => h(App),
+        store
+});
+
+window.store = store;
+window.vm = vm;
+
+store.dispatch('retrieveWeights');
